6b5ae535854066328c2d593ed462fde1907bf142
wan <wanbx@sysom.com.cn>
2016-07-15 14:22:02 +0800

    wan

--- tbox_user/Application/Api/Conf/config.php
+++ tbox_user/Application/Api/Conf/config.php
@@ -3,16 +3,7 @@ define('ip', 'http://121.40.133.69');
 define('layout_time',3);
 
 $api_config=array(    
-		'URL_ROUTER_ON'   => true,
-		'DATA_CACHE_TYPE'       => 'Redis',  // 数据缓存类型,
-		'REDIS_HOST'=>'127.0.0.1', //redis服务器ip，多台用逗号隔开；读写分离开启时，第一台负责写，其它[随机]负责读；
-		'REDIS_PORT'=>'6379',//端口号
-		'REDIS_PERSISTENT'=>false,//是否长连接 false=短连接
-		'DATA_CACHE_PREFIX'=> 'Redis_',
-		'REDIS_RW_SEPARATE' => false, //Redis读写分离 true 开启
-		'REDIS_TIMEOUT'=>'300',//超时时间
-		'REDIS_PASSWORD'=>'',//Redis认证密码
-		'DATA_CACHE_TIME'=> 10800,// 数据缓存有效期 0表示永久缓存
+		
 		'URL_ROUTE_RULES'=>array(
 		array('device/:device_id/sensor/:sensor_id/json','datapoint/history','',array('method'=>'GET')),
 		array('device/:device_id/sensor/:sensor_id/datapoint/:key','datapoint/read','',array('method'=>'GET')),
