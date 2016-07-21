7aa0c07d76506f2e556f67f2fe057ddde12b7019
chensxsysom <chensx@sysom.com.cn>
2016-07-21 14:06:02 +0800

    aa

--- tbox_user/Public/js/websockets.js
+++ tbox_user/Public/js/websockets.js
@@ -4,10 +4,13 @@ var wsServer = 'ws://192.168.1.42:8283'; //服务器地址
 var websocket = new WebSocket(wsServer); //创建WebSocket对象
 /*websocket.send("hello");//向服务器发送消息*/
 alert(websocket.readyState);//查看websocket当前状态
-alert($.cookie('the_cookie'); // 读取 cookie )
+alert($.cookie('token')); // 读取 cookie )
 websocket.onopen = function (evt) {
 	alert(evt);//已经建立连接
+	alert(websocket.readyState);
+	websocket.send( $.cookie('token') );
 };
+
 websocket.onclose = function (evt) {
 	alert(websocket.readyState);//已经关闭连接
 };
@@ -17,4 +20,3 @@ websocket.onmessage = function (evt) {
 websocket.onerror = function (evt) {
 //产生异常
 }; 
-alert(websocket.readyState);
